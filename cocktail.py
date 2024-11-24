1abb5d667c8ef721365cd34b178ec52c293abbf0
Ramandeep Singh <ramandeep-singh25@outlook.com>
2024-11-24 17:27:15 +0100

    first commit

---
+++ cocktail.py
@@ -0,0 +1,24 @@
+'''
+Program: Cockatil Maker
+Author: Ramandeep Singh
+Date: 24/22/2024
+Summary: This program will allow the user to select a cocktail from a list of cocktails and display the recipe for the selected cocktail.
+
+'''
+
+import csv
+
+# Load the data from the CSV file
+with open('final_cocktails.csv', 'r') as file:
+    cocktail_data = csv.reader(file)
+
+    # Skip the header row
+    next(cocktail_data)
+
+    #Count the number of disinct cocktails
+    cocktail_count = 0
+    for row in cocktail_data:
+        cocktail_count += 1
+
+# Display the total number of cocktails
+print(f'Total number of cocktails: {cocktail_count}')
